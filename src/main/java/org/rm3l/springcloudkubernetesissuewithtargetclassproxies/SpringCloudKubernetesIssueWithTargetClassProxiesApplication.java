33c12f1d6dca43a1aa30329cef7ae2e4e28f1894
Armel Soro <armel@rm3l.org>
2021-04-30 23:47:20 +0200

    Initial commit

---
+++ src/main/java/org/rm3l/springcloudkubernetesissuewithtargetclassproxies/SpringCloudKubernetesIssueWithTargetClassProxiesApplication.java
@@ -0,0 +1,13 @@
+package org.rm3l.springcloudkubernetesissuewithtargetclassproxies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKubernetesIssueWithTargetClassProxiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudKubernetesIssueWithTargetClassProxiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rm3l/springcloudkubernetesissuewithtargetclassproxies/SpringCloudKubernetesIssueWithTargetClassProxiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rm3l.springcloudkubernetesissuewithtargetclassproxies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKubernetesIssueWithTargetClassProxiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
